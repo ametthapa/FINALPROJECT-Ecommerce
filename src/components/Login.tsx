f265888b17ea630f63ac736f4423b1c58d053d6e
Amrit Thapa <thapa.amet27@gmail.com>
2022-03-27 18:31:39 +0545

    done for login

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,5 +1,27 @@
+import {useState, ChangeEvent } from "react";
+
 import { Link } from "react-router-dom";
+import axios from "axios";
+
 const Login = () =>{
+    const [email,setEmail] = useState<string>("");
+    const [password, setPassword] = useState<string>("");
+
+    const postRequestHandler = async (e:any) =>{
+        e.preventDefault();
+        const data={
+            email,
+            password
+        }
+        console.log(data)
+
+        const response = await axios.post("https://uat.ordering-boafresh.ekbana.net/api/v4/auth/login",data)
+
+        console.log(response);
+    }
+
+
+
     return (
         <div>
             <div className="login">
@@ -8,12 +30,12 @@ const Login = () =>{
                 
                     <div className="login-form-grids animated wow slideInUp" data-wow-delay=".5s">
                         <form>
-                            <input type="email" placeholder="Email Address" required />
-                            <input type="password" placeholder="Password" required />
+                            <input type="email" placeholder="Email Address" value={email} name={email} onChange={(e) => setEmail(e.target.value)} required />
+                            <input type="password" placeholder="Password" value={password} name={password} onChange={(e) => setPassword(e.target.value)} required />
                             <div className="forgot">
                                 <a href="#">Forgot Password?</a>
                             </div>
-                            <input type="submit" value="Login" />
+                            <input type="submit" value="Login" onClick={(e) => postRequestHandler(e)} />
                         </form>
                     </div>
                     <h4>For New People</h4>
