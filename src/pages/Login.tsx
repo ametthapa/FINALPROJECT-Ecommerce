b9106254ec9c62b9ca9d1f984a3ca3d24af0661a
Amrit Thapa <thapa.amet27@gmail.com>
2022-04-02 14:03:51 +0545

    login done

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,79 +1,93 @@
-
-import {useState } from "react";
-
 import { Link } from "react-router-dom";
-import axios from "axios";
 import Breadcrumbs from "../components/Breadcrumbs";
 
-const Login = () =>{
-    const client_id=2;
-    const client_secret = "olzBb6we0po4B0PSJyDpNGhhSsnvZmeio8sRoASa";
-    const grant_type ="password"
-    const [username,setEmail] = useState<string>("");
-    const [password, setPassword] = useState<string>("");
+const Login =()=> {
+const clientSecret:string = "olzBb6we0po4B0PSJyDpNGhhSsnvZmeio8sRoASa";
+const ClientId:string = "2";
 
-    const postRequestHandler = async (e:any) =>{
-        e.preventDefault();
-        const data={
-            client_id,
-            client_secret,
-            grant_type,
-            username,
-            password,
-            email:username,
+  const handleLogin=async (e: any)=> {
+    e.preventDefault();
+    // const formData = new FormData(e);
+    const loginData = {
+      grant_type: "password",
+      email: e.target.email.value,
+      username: e.target.email.value,
+      client_id: ClientId,
+      client_secret: clientSecret,
+      password: e.target.password.value,
+    };
+
+    fetch("https://uat.ordering-dalle.ekbana.net/api/v4/auth/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(loginData),
+    })
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        if (data.expires_in) {
+          alert(
+            "You are logged in successfully."
+          );
+        } else {
+          alert("Login failed!");
         }
-        console.log(data)
-
-        // const response = await axios.post("https://uat.ordering-boafresh.ekbana.net/api/v4/auth/login",data)
-
-        // console.log(response);
-        fetch("https://uat.ordering-boafresh.ekbana.net/api/v4/auth/login", {
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json",
-            },
-            body: JSON.stringify(data),
-          })
-            .then((response) => {
-              return response.json();
-            })
-            .then((data) => {
-              if (data.expires_in) {
-                alert(
-                  "Your are logged in successfully. Your session will expire after " +
-                    data.expires_in / (3600 * 24) +
-                    " days."
-                );
-                window.location.href = "/";
-              } else {
-                alert("Login failed!");
-              }
-              console.log(data);
-            });
-      
-    }
+        localStorage.setItem('access_token',data.access_token);
+      });
+  } 
     return (
-        <div>
-            <Breadcrumbs name="Login Page" />
-            <div className="login">
-                <div className="container">
-                    <h2>Login Form</h2>
-                
-                    <div className="login-form-grids animated wow slideInUp" data-wow-delay=".5s">
-                        <form>
-                            <input type="email" placeholder="Email Address" value={username} name={username} onChange={(e) => setEmail(e.target.value)} required />
-                            <input type="password" placeholder="Password" value={password} name={password} onChange={(e) => setPassword(e.target.value)} required />
-                            <div className="forgot">
-                                <Link to="/forgotpassword">Forgot Password?</Link>
-                            </div>
-                            <input type="submit" value="Login" onClick={(e) => postRequestHandler(e)} />
-                        </form>
-                    </div>
-                    <h4>For New People</h4>
-                    <p><Link to="/register">Register Here</Link> (Or) go back to <Link to="/">Home<span className="glyphicon glyphicon-menu-right" aria-hidden="true"></span></Link></p>
+      <>
+        <Breadcrumbs name="Login Page" />
+        {/* </>login */}
+        <div className="login">
+          <div className="container">
+            <h2>Login Form</h2>
+
+            <div
+              className="login-form-grids animated wow slideInUp"
+              data-wow-delay=".5s"
+            >
+              <form onSubmit={handleLogin}>
+                <input
+                  type="email"
+                  id="email"
+                  name="email"
+                  placeholder="Email Address"
+                  required
+                />
+                <input
+                  type="password"
+                  id="password"
+                  name="password"
+                  placeholder="Password"
+                  required
+                />
+                <div className="forgot">
+                  <div>
+                    <b>Forgot Password?</b>
+                  </div>
                 </div>
+                <input type="submit" value="Login" />
+              </form>
             </div>
+            <h4>For New People</h4>
+            <p>
+              <Link to="/registered">Register Here</Link> (Or) go back to{" "}
+              <Link to="/">
+                Home
+                <span
+                  className="glyphicon glyphicon-menu-right"
+                  aria-hidden="true"
+                ></span>
+              </Link>
+            </p>
+          </div>
         </div>
-    )
-}
+      </>
+    );
+  }
+
 export default Login;
